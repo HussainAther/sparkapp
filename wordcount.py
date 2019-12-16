415b3240999e2094e832a15180627d050539a44c
Syed Hussain Ather <shussainather@gmail.com>
2019-12-16 00:52:42 -0800

    words

--- wordcount.py
+++ wordcount.py
@@ -9,6 +9,10 @@ Word count for input file.
 
 infile = sys.argv[1]
 
-prnit("Number of lines in the file: %s" %  infile.count())
+print("Number of lines in the file: %s" %  infile.count())
 
-
+# Get the words.
+words = infile.flatMap(lambda line: re.split("\W+", line.lower().strip()))
+words = words.filter(lambda x: len(x) > 3) # words more than 3 characters
+words = words.map(lambda w: (w,1)) # count 1 per word
+worsd = words.reduceByKey(add) # Sum count all words.
