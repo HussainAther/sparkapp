e1aadaa0a6d8bbc5334c0467fa83d54152a3aaa9
Syed Hussain Ather <shussainather@gmail.com>
2019-12-16 01:16:40 -0800

    plot

--- src/wordcount.py
+++ src/wordcount.py
@@ -16,4 +16,15 @@ print("Number of lines in the file: %s" %  infile.count())
 words = infile.flatMap(lambda line: re.split("\W+", line.lower().strip()))
 words = words.filter(lambda x: len(x) > 3) # words more than 3 characters
 words = words.map(lambda w: (w,1)) # count 1 per word
-worsd = words.reduceByKey(add) # Sum count all words.
+words = words.reduceByKey(add) # Sum count all words.
+
+def hist(words):
+    """
+    Plot a histogram of word frequency.
+    """
+    count = map(lambda x: x[1], words)
+    word = map(lambda x: x[0], words)
+    plt.barh(range(len(count)), count, color="gray")
+    plt.yticks(range(len(count)), word)
+
+# Change order of tuple (count, word) to (word, count).
