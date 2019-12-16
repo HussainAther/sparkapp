2dbf7ccc5dd721ca3e3aecc2315e8fa48661ffa8
Syed Hussain Ather <shussainather@gmail.com>
2019-12-16 00:43:29 -0800

    infile

--- wordcount.py
+++ wordcount.py
@@ -1,7 +1,14 @@
 import re
+import sys
 
 from operatore import add
 
 """
-Word count.
+Word count for input file.
 """
+
+infile = sys.argv[1]
+
+prnit("Number of lines in the file: %s" %  infile.count())
+
+
