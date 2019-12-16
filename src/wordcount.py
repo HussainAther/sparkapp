d95646f03dd2b4bddf133162145d2baf9644d111
Syed Hussain Ather <shussainather@gmail.com>
2019-12-16 02:46:04 -0800

    no func

--- src/wordcount.py
+++ src/wordcount.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import re
 import sys
 
-from pyspark import SparkContext
 from operator import add
+from pyspark import SparkContext
 
 """
 Word count for input file.
@@ -20,19 +21,16 @@ words = words.filter(lambda x: len(x) > 3) # words more than 3 characters
 words = words.map(lambda w: (w,1)) # count 1 per word
 words = words.reduceByKey(add) # Sum count all words.
 
-def hist(words):
-    """
-    Plot a histogram of word frequency.
-    """
-    count = map(lambda x: x[1], words)
-    word = map(lambda x: x[0], words)
-    plt.barh(range(len(count)), count, color="gray")
-    plt.yticks(range(len(count)), word)
-
 # Change order of tuple (count, word) to (word, count).
 words = words.map(lambda x:(x[1], x[0]))
-words.take(25)
+w = words.take(25)
 
-# Display the histogram.
-hist(words.take(25))
-
+"""
+Plot a histogram of word frequency.
+"""
+count = list(map(lambda x: x[1], w))
+word = list(map(lambda x: x[0], w))
+x = range(len(count))
+plt.barh(x, count, color="gray")
+plt.yticks(x, word)
+plt.savefig("output/mobydick.png")
